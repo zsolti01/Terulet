1427b8e96d2983c82b46b704df58274eca283621
Vécsei Zsolt <VecseiZs@KKSZKI.LOCAL>
2024-09-20 13:08:49 +0200

    The result is completed. :D

--- Program.cs
+++ Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.AccessControl;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -16,9 +17,23 @@ namespace Terulet
             int b;
             int T;
 
-            a = Convert.ToInt32(Console.ReadLine());
-            b = Convert.ToInt32(Console.ReadLine());
+            do
+            {
+                Console.WriteLine("Kérem az 'a' oldal értékét (0-nál nagyobb).");
+                a = Convert.ToInt32(Console.ReadLine());
+            } while (a <= 0);
+
+            do
+            {
+                Console.WriteLine("Kérem a 'b' oldal értékét (0-nál nagyobb)");
+                b = Convert.ToInt32(Console.ReadLine());
+            } while (b <= 0);
+
             T = a * b;
+
+            //Console.WriteLine("A téglalap területe: {0}", T);
+            Console.WriteLine($"A téglalap területe: {T}");
+
         }
         static void Main(string[] args)
         {
